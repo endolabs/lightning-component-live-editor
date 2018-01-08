3c7b6faf9c98514424fdb659fe3821483985e7bd
Takumi Endo <tq-ed@v7.com>
2018-01-08 17:59:39 +0900

    destroy components to avoid memory leaks

--- force-app/main/default/aura/dein_Content/dein_ContentController.js
+++ force-app/main/default/aura/dein_Content/dein_ContentController.js
@@ -1,14 +1,20 @@
 ({
-    answer : function(component, event, helper) {
+    initItem : function(component, event, helper) {
         var text = event.getParam("selectedItem");
+        
+        // Destroy a previous component to avoid memory leaks.
+        var current = component.get("v.body");
+        if (current) {
+            current.destroy();
+        }
         
         $A.createComponent(
             "c:dein_Item_" + text,
             {
             },
-            function(newButton, status, errorMessage){
+            function(createdComponent, status, errorMessage){
                 if (status === "SUCCESS") {
-                    component.set("v.body", newButton);
+                    component.set("v.body", createdComponent);
                 }
                 else if (status === "INCOMPLETE") {
                     component.set("No response from server or client is offline.")
