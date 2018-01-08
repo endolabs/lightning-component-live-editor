14d3e418b1301145947f937745dbbb0ca2feecad
Takumi Endo <tq-ed@v7.com>
2018-01-08 16:47:32 +0900

    Add lightning:spinner

--- force-app/main/default/aura/dein_Item_Spinner/dein_Item_SpinnerController.js
+++ force-app/main/default/aura/dein_Item_Spinner/dein_Item_SpinnerController.js
@@ -1,5 +1,16 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+    update : function(component, event, helper) {
+        var codeInfo = helper.codeInfo(component);
+       
+        var code = codeInfo.code;
+        component.set("v.code", code);
+        
+        component.set("v.toggleSpinner", true);         
+        helper.create(component, codeInfo.componentName, codeInfo.componentAttributes);
+        helper.renderCode(component);
+        
+        setTimeout(function() {
+            component.set("v.toggleSpinner", false); 
+        }, 1000);
+    },
 })
--- force-app/main/default/aura/dein_Item_Spinner/dein_Item_SpinnerHelper.js
+++ force-app/main/default/aura/dein_Item_Spinner/dein_Item_SpinnerHelper.js
@@ -1,5 +1,19 @@
 ({
-	helperMethod : function() {
-		
-	}
+    codeInfo : function(component) {
+        return component.find("util").parseTemplate('lightning:spinner', {
+            '@alternativeText': component.get("v.alternativeText"),
+            '@size': component.get("v.size"),
+            '@title': component.get("v.title"),
+            '@variant': component.get("v.variant")
+        });
+    },
+    
+    create : function(component, name, attributes) {
+        component.find("preview").createComponent(component, name, attributes);
+    },
+    
+    renderCode : function(component) {
+        console.log("button render code.");
+        component.find("highlight").highlightCode(component.get("v.code"));
+    }
 })
