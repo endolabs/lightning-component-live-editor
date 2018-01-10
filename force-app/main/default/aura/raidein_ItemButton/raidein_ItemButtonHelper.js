6b162afa2fd8a6afa222f1e0da1a4f14f5b47d27
Takumi Endo <tq-ed@v7.com>
2018-01-10 23:13:18 +0900

    code format

--- force-app/main/default/aura/raidein_ItemButton/raidein_ItemButtonHelper.js
+++ force-app/main/default/aura/raidein_ItemButton/raidein_ItemButtonHelper.js
@@ -1,17 +1,17 @@
 ({
-    componentSetting : function(component) {
-        return component.find("util").parseTemplate('lightning:button', {
-            '@label': component.get("v.label"),
-            '@variant': component.get("v.variant")
-        });
-    },
-    
-    create : function(component, name, attributes) {
-        component.find("preview").createComponent(component, name, attributes);
-    },
-    
-    renderCode : function(component) {
-        console.log("button render code.");
-        component.find("highlight").highlightCode(component.get("v.code"));
-    }
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:button', {
+      '@label': component.get("v.label"),
+      '@variant': component.get("v.variant")
+    });
+  },
+
+  create: function (component, name, attributes) {
+    component.find("preview").createComponent(component, name, attributes);
+  },
+
+  renderCode: function (component) {
+    console.log("button render code.");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
