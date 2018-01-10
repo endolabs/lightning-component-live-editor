05264319cab4344ca0c435eee7c714819ebe43b4
Takumi Endo <tq-ed@v7.com>
2018-01-10 23:30:19 +0900

    Add lightning:textarea

--- force-app/main/default/aura/raidein_ItemTextArea/raidein_ItemTextAreaHelper.js
+++ force-app/main/default/aura/raidein_ItemTextArea/raidein_ItemTextAreaHelper.js
@@ -1,5 +1,29 @@
 ({
-	helperMethod : function() {
-		
-	}
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:textarea', {
+      '@disabled': component.get("v.disabled"),
+      '@label': component.get("v.label"),
+      "@maxlength": component.get("v.maxlength"),
+      "@messageWhenBadInput": component.get("v.messageWhenBadInput"),
+      "@messageWhenTooLong": component.get("v.messageWhenTooLong"),
+      "@messageWhenValueMissing": component.get("v.messageWhenValueMissing"),
+      "@minlength": component.get("v.minlength"),
+      "@name": component.get("v.name"),
+      "@placeholder": component.get("v.placeholder"),
+      "@readonly": component.get("v.readonly"),
+      "@required": component.get("v.required"),
+      "@title": component.get("v.title"),
+      "@value": component.get("v.value"),
+      '@variant': component.get("v.variant")
+    });
+  },
+
+  create: function (component, name, attributes) {
+    component.find("preview").createComponent(component, name, attributes);
+  },
+
+  renderCode: function (component) {
+    console.log("button render code.");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
