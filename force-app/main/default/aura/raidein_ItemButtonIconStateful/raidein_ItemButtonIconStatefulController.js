2f52c38e25839311fb54bb7bdc79ef84c9e4e23d
Takumi Endo <tq-ed@v7.com>
2018-01-10 22:46:42 +0900

    Implement lightning:buttonIconStateful

--- force-app/main/default/aura/raidein_ItemButtonIconStateful/raidein_ItemButtonIconStatefulController.js
+++ force-app/main/default/aura/raidein_ItemButtonIconStateful/raidein_ItemButtonIconStatefulController.js
@@ -1,6 +1,19 @@
 ({
-	handleToggle : function(component, event, helper) {
-        var liked = component.get("v.liked");
-        component.set("v.liked", !liked);
-	}
+  handleClick: function (component, event, helper) {
+    var buttonSelected = component.get('v.selected');
+    component.set('v.selected', !buttonSelected);
+  },
+
+  init: function (component, event, helper) {
+    var iconNames = component.find("util").getUtilityIconNames().map(function (icon) {
+      return 'utility:' + icon;
+    });
+
+    component.set("v.iconNames", iconNames);
+    helper.update(component);
+  },
+
+  update: function (component, event, helper) {
+    helper.update(component);
+  },
 })
--- force-app/main/default/aura/raidein_ItemButtonIconStateful/raidein_ItemButtonIconStatefulHelper.js
+++ force-app/main/default/aura/raidein_ItemButtonIconStateful/raidein_ItemButtonIconStatefulHelper.js
@@ -1,5 +1,30 @@
 ({
-	helperMethod : function() {
-		
-	}
+  update: function (component) {
+    var componentSetting = this.componentSetting(component);
+
+    var code = componentSetting.code;
+    component.set("v.code", code);
+
+    this.create(component, componentSetting.componentName, componentSetting.componentAttributes);
+    this.renderCode(component);
+  },
+
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:buttonIconStateful', {
+      '@alternativeText': component.get("v.alternativeText"),
+      '@selected': component.get("v.selected"),
+      '@iconName': component.get("v.iconName"),
+      '@size': component.get("v.size"),
+      '@variant': component.get("v.variant")
+    });
+  },
+
+  create: function (component, name, attributes) {
+    component.find("preview").createComponent(component, name, attributes);
+  },
+
+  renderCode: function (component) {
+    console.log("button render code.");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
