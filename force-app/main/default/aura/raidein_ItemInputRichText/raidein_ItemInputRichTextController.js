c6fcfbb10fa6e6ec19a44f63ff49455f4efd078a
Takumi Endo <tq-ed@v7.com>
2018-01-11 00:09:41 +0900

    Add lightning:inputRechText

--- force-app/main/default/aura/raidein_ItemInputRichText/raidein_ItemInputRichTextController.js
+++ force-app/main/default/aura/raidein_ItemInputRichText/raidein_ItemInputRichTextController.js
@@ -1,5 +1,11 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+  update: function (component, event, helper) {
+    var componentSetting = helper.componentSetting(component);
+
+    var code = componentSetting.code;
+    component.set("v.code", code);
+
+    helper.create(component, componentSetting.componentName, componentSetting.componentAttributes);
+    helper.renderCode(component);
+  },
 })
--- force-app/main/default/aura/raidein_ItemInputRichText/raidein_ItemInputRichTextHelper.js
+++ force-app/main/default/aura/raidein_ItemInputRichText/raidein_ItemInputRichTextHelper.js
@@ -1,5 +1,22 @@
 ({
-	helperMethod : function() {
-		
-	}
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:inputRichText', {
+      '@disabled': component.get("v.disabled"),
+      "@messageWhenBadInput": component.get("v.messageWhenBadInput"),
+      "@placeholder": component.get("v.placeholder"),
+      "@title": component.get("v.title"),
+      "@value": component.get("v.value"),
+      "@disabledCategories": "FORMAT_TEXT", // TODO
+      '@variant': component.get("v.variant")
+    });
+  },
+
+  create: function (component, name, attributes) {
+    component.find("preview").createComponent(component, name, attributes);
+  },
+
+  renderCode: function (component) {
+    console.log("button render code.");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
