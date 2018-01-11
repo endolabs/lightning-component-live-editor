44fae792bfa6f38133d59a530a5b7d127305bd24
Takumi Endo <tq-ed@v7.com>
2018-01-12 00:28:57 +0900

    update tab

--- force-app/main/default/aura/raidein_ItemTab/raidein_ItemTabController.js
+++ force-app/main/default/aura/raidein_ItemTab/raidein_ItemTabController.js
@@ -1,13 +1,9 @@
 ({
-    tabSelected: function(component,event,helper) {
-        console.log(component.get("v.selTabId"));
-    },
-    
-    update: function(component, event, helper) {
-        var componentSetting = helper.componentSetting(component);
-        component.set("v.code", componentSetting.code);
-        
-        helper.create(component, componentSetting);
-        helper.renderCode(component);
-    }
+  update: function (component, event, helper) {
+    var componentSetting = helper.componentSetting(component);
+    component.set("v.code", componentSetting.code);
+
+    helper.create(component, componentSetting);
+    helper.renderCode(component);
+  }
 })
--- force-app/main/default/aura/raidein_ItemTab/raidein_ItemTabHelper.js
+++ force-app/main/default/aura/raidein_ItemTab/raidein_ItemTabHelper.js
@@ -1,39 +1,45 @@
 ({
-	getTemplate : function(component) {
-        return {
-            "@selectedTabId": component.get("v.selTabId"),
-            "lightning:tab": [
-                {
-                    "@aura:id": "tab1",
-                    "@tabindex": "1",
-                    "@id": "tab1",
-                    "@title": "Tab 1",
-                    "@label": "Tab 1",
-                    "aura:text": { "@value": "First Tab" }
-                },
-                {
-                    "@aura:id": "tab2",
-                    "@tabindex": "2",
-                    "@id": "tab2",
-                    "@title": "Tab 2",
-                    "@label": "Tab 2",
-                    "aura:text": { "@value": "Second Tab" }
-                }
-            ]
-        };
-	},
+  getTemplate: function (component) {
+    return {
+      "@selectedTabId": component.get("v.selTabId"),
+      "lightning:tab": [
+        {
+          "@aura:id": "tab1",
+          "@tabindex": "1",
+          "@id": "tab1",
+          "@title": "Tab 1",
+          "@label": "Tab 1",
+          "aura:text": { "@value": "First Tab" }
+        },
+        {
+          "@aura:id": "tab2",
+          "@tabindex": "2",
+          "@id": "tab2",
+          "@title": "Tab 2",
+          "aura:set": {
+            "@attribute": "label",
+            "aura:text": { "@value": "Tab 2" },
+            "lightning:icon": { "@iconName": "utility:connected_apps" }
+          },
+          "aura:text": { "@value": "Second Tab" }
+        }
+      ]
+    };
+  },
 
-    componentSetting: function(component) {
-        var template = this.getTemplate(component);
-        return component.find("util").parseTemplate("lightning:layout", template);
-    },
-    
-    create: function(component, componentSetting) {
-        // component.find("preview").createComponents(component, componentSetting);
-    },
-    
-    renderCode : function(component) {
-        console.log("render code");
-        component.find("highlight").highlightCode(component.get("v.code"));
-    }
+  componentSetting: function (component) {
+    var template = this.getTemplate(component);
+    return component.find("util").parseTemplate("lightning:tabset", template);
+  },
+
+  create: function (component, componentSetting) {
+    // FIXME: template cause exception!
+    // aura://ComponentController: java.lang.ClassCastException: java.lang.String cannot be cast to java.util.List
+    // component.find("preview").createComponents(component, componentSetting);
+  },
+
+  renderCode: function (component) {
+    console.log("render code");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
