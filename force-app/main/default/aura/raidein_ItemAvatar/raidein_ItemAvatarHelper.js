a6834420e79aa29f599e869f3a92f5fdba47c873
Takumi Endo <tq-ed@v7.com>
2018-01-17 01:28:40 +0900

    add avatar component

--- force-app/main/default/aura/raidein_ItemAvatar/raidein_ItemAvatarHelper.js
+++ force-app/main/default/aura/raidein_ItemAvatar/raidein_ItemAvatarHelper.js
@@ -1,5 +1,21 @@
 ({
-	helperMethod : function() {
-		
-	}
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:avatar', {
+      '@src': component.get("v.src"),
+      '@alternativeText': component.get("v.alternativeText"),
+      '@fallbackIconName': component.get("v.fallbackIconName"),
+      '@initials': component.get("v.initials"),
+      '@size': component.get("v.size"),
+      '@variant': component.get("v.variant")
+    });
+  },
+
+  create: function (component, componentSetting) {
+    component.find("preview").createComponent(component, componentSetting.componentName, componentSetting.componentAttributes);
+  },
+
+  renderCode: function (component) {
+    console.log("button render code.");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
