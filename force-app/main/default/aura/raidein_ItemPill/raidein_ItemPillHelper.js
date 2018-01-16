65f9576160557b5f267a0b7971f12ac29b597616
Takumi Endo <tq-ed@v7.com>
2018-01-17 01:39:07 +0900

    add pill component

--- force-app/main/default/aura/raidein_ItemPill/raidein_ItemPillHelper.js
+++ force-app/main/default/aura/raidein_ItemPill/raidein_ItemPillHelper.js
@@ -1,5 +1,29 @@
 ({
-	helperMethod : function() {
-		
-	}
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:pill', {
+      '@label': component.get("v.label"),
+      '@hasError': component.get("v.hasError"),
+      '@href': component.get("v.href"),
+      '@name': component.get("v.name"),
+      '@title': component.get("v.title"),
+      // TODO: make option whether use media or not
+      // '@media': component.get("v.media"),
+      'aura:set': {
+        '@attribute': 'media',
+        'lightning:icon': {
+          '@iconName': 'standard:account',
+          '@alternativeText': 'Account'
+        }
+      }
+    });
+  },
+
+  create: function (component, componentSetting) {
+    component.find("preview").createComponent(component, componentSetting.componentName, componentSetting.componentAttributes);
+  },
+
+  renderCode: function (component) {
+    console.log("button render code.");
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
