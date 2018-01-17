c6713563098ed8fb95f02cd2fd1de8049c1c7093
Takumi Endo <tq-ed@v7.com>
2018-01-18 00:57:07 +0900

    add dynamic icon component

--- force-app/main/default/aura/raidein_ItemDynamicIcon/raidein_ItemDynamicIconController.js
+++ force-app/main/default/aura/raidein_ItemDynamicIcon/raidein_ItemDynamicIconController.js
@@ -1,5 +1,14 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+  update: function (component, event, helper) {
+    var eventSourceName = event.getSource().get("v.name");
+    if (eventSourceName === 'type') {
+      helper.updateOptions(component);
+    }
+
+    var componentSetting = helper.componentSetting(component);
+    component.set("v.code", componentSetting.code);
+
+    helper.create(component, componentSetting);
+    helper.renderCode(component);
+  },
 })
--- force-app/main/default/aura/raidein_ItemDynamicIcon/raidein_ItemDynamicIconHelper.js
+++ force-app/main/default/aura/raidein_ItemDynamicIcon/raidein_ItemDynamicIconHelper.js
@@ -1,5 +1,40 @@
 ({
-	helperMethod : function() {
-		
-	}
+  updateOptions: function (component) {
+    var options = [];
+    var defaultValue = "";
+
+    var type = component.get("v.type");
+    if (type === 'eq') {
+      options = ['play', 'stop'];
+      defaultValue = 'play';
+    } else if (type === 'score') {
+      options = ['positive', 'negative'];
+      defaultValue = 'positive';
+    } else if (type === 'strength') {
+      options = ['-3', '-2', '-1', '0', '1', '2', '3'];
+      defaultValue = '0'
+    } else if (type === 'trend') {
+      options = ['neutral', 'up', 'down'];
+      defaultValue = 'neutral';
+    }
+
+    component.set("v.options", options);
+    component.set("v.option", defaultValue);
+  },
+
+  componentSetting: function (component) {
+    return component.find("util").parseTemplate('lightning:dynamicIcon', {
+      '@type': component.get("v.type"),
+      '@option': component.get("v.option"),
+      '@alternativeText': component.get("v.alternativeText")
+    });
+  },
+
+  create: function (component, componentSetting) {
+    component.find("preview").createComponent(component, componentSetting.componentName, componentSetting.componentAttributes);
+  },
+
+  renderCode: function (component) {
+    component.find("highlight").highlightCode(component.get("v.code"));
+  }
 })
